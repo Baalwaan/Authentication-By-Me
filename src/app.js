75faae47efc8b925c871479d0f3eb4ac0f64b95e
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-05-22 20:44:24 +0100

    express server setup and middlewares

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
-isadhoais
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var 
+// const controllers = require('./views/index');
+
+const app = express();
+
+// middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+  }),
+);
+
+app.set('port', process.env.PORT || 3000);
+// app.use(controllers);
+
+module.exports = app;
