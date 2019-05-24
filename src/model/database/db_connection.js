9b1a56d508962cdd989e83f2e8772addfff576a8
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-05-24 20:17:57 +0100

    db_connection.js created

---
+++ src/model/database/db_connection.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+const { readFileSync } = require('fs');
+
+require('env2')('./config.env');
+
+
+let connectionString = process.env.DATABASE_URL;
+
+if (process.env.NODE_ENV === 'test') {
+    connectionString = process.env.TEST_DATABASE_URL;
+};
+
+if (!connectionString) {
+    throw new Error('No DATABASE_URL. Please set DATABASE_URL')
+};
+
+module.exports = new Pool({
+    connectionString,
+    max: 2,
+    ssl: true
+});
