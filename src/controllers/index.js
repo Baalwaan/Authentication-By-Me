2f987a8bd0694f2f5adaa65f0e4845feaeef374c
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-05-24 17:50:28 +0100

    form result is now being picked up in backend by router

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,10 +7,14 @@ const router = express.Router();
 // controllers
 const home = require('./home');
 const signUp = require('./signUp');
+const postUser = require('./postUser');
 
 
 // routes
 router.get('/', home);
 router.get('/signup', signUp);
+
+
+router.post('/postuser', postUser)
 
 module.exports = router;
---
+++ src/controllers/postUser.js
@@ -0,0 +1,7 @@
+const postUser = (req, res) => {
+    console.log(req.body);
+    res.end()
+    // res.render('postUser');
+};
+
+module.exports = postUser;
