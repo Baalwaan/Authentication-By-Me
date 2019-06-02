0bb5e186f8f0da69e003c0ec5f7a92a884ba3cf8
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-06-02 20:01:56 +0100

    console.log changed in comparePassword.js

--- src/controllers/authentication/comparePassword.js
+++ src/controllers/authentication/comparePassword.js
@@ -1,14 +1,15 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const comparePassword = (password, hashed_password) => new Promise((resolve, reject) => {
-  bcrypt.compare(password, hashed_password).then((response) => {
-    if (response) {
-      resolve(true);
-    } else {
-      console.log('wrongpassword do not match');
-      reject('Account or password is incorrect.');
-    }
+const comparePassword = (password, hashed_password) =>
+  new Promise((resolve, reject) => {
+    bcrypt.compare(password, hashed_password).then(response => {
+      if (response) {
+        resolve(true);
+      } else {
+        console.log("Account or password is incorrect.");
+        reject("Account or password is incorrect.");
+      }
+    });
   });
-});
 
 module.exports = comparePassword;
