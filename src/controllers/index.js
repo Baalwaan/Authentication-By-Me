d0387c155733103b004f1d23cf0aa4b665c2c6d9
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-06-02 19:43:32 +0100

    filepath to logOut controller fixed

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
+const express = require("express");
 // const path = require('path');
 
 // initialising express router
 const router = express.Router();
 
 // controllers
-const home = require('./home');
-const signUp = require('./signUp');
-const postUser = require('./postUser');
-const login = require('./login');
-const loginPage = require('./loginPage');
-const logOut = require('./logout');
+const home = require("./home");
+const signUp = require("./signUp");
+const postUser = require("./postUser");
+const login = require("./login");
+const loginPage = require("./loginPage");
+const logOut = require("./logOut");
 // const checkToken = require("./checkToken");
 // for testing
-const profile = require('./profile');
+const profile = require("./profile");
 
 // routes
-router.get('/', home);
-router.get('/signup', signUp);
-router.get('/loginpage', loginPage);
+router.get("/", home);
+router.get("/signup", signUp);
+router.get("/loginpage", loginPage);
 
 // if endpoint is profile
 
-router.get('/logout', logOut);
+router.get("/logout", logOut);
 
-router.post('/postuser', postUser);
-router.post('/login', login);
+router.post("/postuser", postUser);
+router.post("/login", login);
 
 // testing endpoint
 // router.get("/google", home);
